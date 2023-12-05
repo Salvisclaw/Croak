1294d749be50335993efa86c5e0f55b72175b19d
Salvisclaw <88795453+Salvisclaw@users.noreply.github.com>
2023-12-05 11:18:07 -0600

    Update main.py

--- main.py
+++ main.py
@@ -432,56 +432,56 @@ def result_page():
          if i%5==0:
             time.sleep(1)
 def About_page():
-st.header('Development')
-    """
-    Made by Tator19 
-    Massive thanks to RedValis for being the very base of this project
-    """
-    st.subheader('Spotify Million Playlist Dataset')
-    """
-    We're using the Million Playlist Dataset, which, as its name implies, consists of one million playlists.
-    contains a number of songs, and some metadata is included as well, such as the name of the playlist, duration, number of songs, number of artists, etc.
-    This allows for a great accuracy score and a larger number of songs scored according to how people organize their public playlists
-    """
-
-    """
-    It is created by sampling playlists from the billions of playlists that Spotify users have created over the years. 
-    Playlists that meet the following criteria were selected at random:
-    - Created by a user that resides in the United States and is at least 13 years old
-    - Was a public playlist at the time the MPD was generated
-    - Contains at least 10 tracks
-    - Contains no more than 300 tracks
-    - Contains at least 3 unique artists
-    - Contains at least 2 unique albums
-    - Has no local tracks (local tracks are non-Spotify tracks that a user has on their local device
-    - Has at least one follower (not including the creator
-    - Was created after January 1, 2010 and before December 1, 2020
-    - Does not have an offensive title
-    - Does not have an adult-oriented title if the playlist was created by a user under 18 years of age
-    Information about the Dataset [here](https://www.aicrowd.com/challenges/spotify-million-playlist-dataset-challenge)
-    """
-    st.subheader('Audio Features Explanation')
-    """
-    This is the list of audio features that we use to determine the similarity between songs.
-    For anyone who would like to inspect the source code 
+    st.header('Development')
+        """
+        Made by Tator19 
+        Massive thanks to RedValis for being the very base of this project
+        """
+        st.subheader('Spotify Million Playlist Dataset')
+        """
+        We're using the Million Playlist Dataset, which, as its name implies, consists of one million playlists.
+        contains a number of songs, and some metadata is included as well, such as the name of the playlist, duration, number of songs, number of artists, etc.
+        This allows for a great accuracy score and a larger number of songs scored according to how people organize their public playlists
+        """
     
-    | Variable | Description |
-    | :----: | :---: |
-    | Acousticness | A confidence measure from 0.0 to 1.0 of whether the track is acoustic. 1.0 represents high confidence the track is acoustic. |
-    | Danceability | Danceability describes how suitable a track is for dancing based on a combination of musical elements including tempo, rhythm stability, beat strength, and overall regularity. A value of 0.0 is least danceable and 1.0 is most danceable. |
-    | Energy | Energy is a measure from 0.0 to 1.0 and represents a perceptual measure of intensity and activity. Typically, energetic tracks feel fast, loud, and noisy. For example, death metal has high energy, while a Bach prelude scores low on the scale. Perceptual features contributing to this attribute include dynamic range, perceived loudness, timbre, onset rate, and general entropy. |
-    | Instrumentalness | Predicts whether a track contains no vocals. "Ooh" and "aah" sounds are treated as instrumental in this context. Rap or spoken word tracks are clearly "vocal". The closer the instrumentalness value is to 1.0, the greater likelihood the track contains no vocal content. Values above 0.5 are intended to represent instrumental tracks, but confidence is higher as the value approaches 1.0. |
-    | Key | The key the track is in. Integers map to pitches using standard Pitch Class notation. E.g. 0 = C, 1 = C♯/D♭, 2 = D, and so on. If no key was detected, the value is -1. |
-    | Liveness | Detects the presence of an audience in the recording. Higher liveness values represent an increased probability that the track was performed live. A value above 0.8 provides strong likelihood that the track is live. |
-    | Loudness | The overall loudness of a track in decibels (dB). Loudness values are averaged across the entire track and are useful for comparing relative loudness of tracks. Loudness is the quality of a sound that is the primary psychological correlate of physical strength (amplitude). Values typically range between -60 and 0 db. |
-    | Mode | Mode indicates the modality (major or minor) of a track, the type of scale from which its melodic content is derived. Major is represented by 1 and minor is 0. |
-    | Speechiness | Speechiness detects the presence of spoken words in a track. The more exclusively speech-like the recording (e.g. talk show, audio book, poetry), the closer to 1.0 the attribute value. Values above 0.66 describe tracks that are probably made entirely of spoken words. Values between 0.33 and 0.66 describe tracks that may contain both music and speech, either in sections or layered, including such cases as rap music. Values below 0.33 most likely represent music and other non-speech-like tracks. |
-    | Tempo | The overall estimated tempo of a track in beats per minute (BPM). In musical terminology, tempo is the speed or pace of a given piece and derives directly from the average beat duration. |
-    | Time Signature | An estimated time signature. The time signature (meter) is a notational convention to specify how many beats are in each bar (or measure). The time signature ranges from 3 to 7 indicating time signatures of "3/4", to "7/4". |
-    | Valence | A measure from 0.0 to 1.0 describing the musical positiveness conveyed by a track. Tracks with high valence sound more positive (e.g. happy, cheerful, euphoric), while tracks with low valence sound more negative (e.g. sad, depressed, angry). |
-    
-    Information about features: [here](https://developer.spotify.com/documentation/web-api/reference/#/operations/get-audio-features)
-    """
+        """
+        It is created by sampling playlists from the billions of playlists that Spotify users have created over the years. 
+        Playlists that meet the following criteria were selected at random:
+        - Created by a user that resides in the United States and is at least 13 years old
+        - Was a public playlist at the time the MPD was generated
+        - Contains at least 10 tracks
+        - Contains no more than 300 tracks
+        - Contains at least 3 unique artists
+        - Contains at least 2 unique albums
+        - Has no local tracks (local tracks are non-Spotify tracks that a user has on their local device
+        - Has at least one follower (not including the creator
+        - Was created after January 1, 2010 and before December 1, 2020
+        - Does not have an offensive title
+        - Does not have an adult-oriented title if the playlist was created by a user under 18 years of age
+        Information about the Dataset [here](https://www.aicrowd.com/challenges/spotify-million-playlist-dataset-challenge)
+        """
+        st.subheader('Audio Features Explanation')
+        """
+        This is the list of audio features that we use to determine the similarity between songs.
+        For anyone who would like to inspect the source code 
+        
+        | Variable | Description |
+        | :----: | :---: |
+        | Acousticness | A confidence measure from 0.0 to 1.0 of whether the track is acoustic. 1.0 represents high confidence the track is acoustic. |
+        | Danceability | Danceability describes how suitable a track is for dancing based on a combination of musical elements including tempo, rhythm stability, beat strength, and overall regularity. A value of 0.0 is least danceable and 1.0 is most danceable. |
+        | Energy | Energy is a measure from 0.0 to 1.0 and represents a perceptual measure of intensity and activity. Typically, energetic tracks feel fast, loud, and noisy. For example, death metal has high energy, while a Bach prelude scores low on the scale. Perceptual features contributing to this attribute include dynamic range, perceived loudness, timbre, onset rate, and general entropy. |
+        | Instrumentalness | Predicts whether a track contains no vocals. "Ooh" and "aah" sounds are treated as instrumental in this context. Rap or spoken word tracks are clearly "vocal". The closer the instrumentalness value is to 1.0, the greater likelihood the track contains no vocal content. Values above 0.5 are intended to represent instrumental tracks, but confidence is higher as the value approaches 1.0. |
+        | Key | The key the track is in. Integers map to pitches using standard Pitch Class notation. E.g. 0 = C, 1 = C♯/D♭, 2 = D, and so on. If no key was detected, the value is -1. |
+        | Liveness | Detects the presence of an audience in the recording. Higher liveness values represent an increased probability that the track was performed live. A value above 0.8 provides strong likelihood that the track is live. |
+        | Loudness | The overall loudness of a track in decibels (dB). Loudness values are averaged across the entire track and are useful for comparing relative loudness of tracks. Loudness is the quality of a sound that is the primary psychological correlate of physical strength (amplitude). Values typically range between -60 and 0 db. |
+        | Mode | Mode indicates the modality (major or minor) of a track, the type of scale from which its melodic content is derived. Major is represented by 1 and minor is 0. |
+        | Speechiness | Speechiness detects the presence of spoken words in a track. The more exclusively speech-like the recording (e.g. talk show, audio book, poetry), the closer to 1.0 the attribute value. Values above 0.66 describe tracks that are probably made entirely of spoken words. Values between 0.33 and 0.66 describe tracks that may contain both music and speech, either in sections or layered, including such cases as rap music. Values below 0.33 most likely represent music and other non-speech-like tracks. |
+        | Tempo | The overall estimated tempo of a track in beats per minute (BPM). In musical terminology, tempo is the speed or pace of a given piece and derives directly from the average beat duration. |
+        | Time Signature | An estimated time signature. The time signature (meter) is a notational convention to specify how many beats are in each bar (or measure). The time signature ranges from 3 to 7 indicating time signatures of "3/4", to "7/4". |
+        | Valence | A measure from 0.0 to 1.0 describing the musical positiveness conveyed by a track. Tracks with high valence sound more positive (e.g. happy, cheerful, euphoric), while tracks with low valence sound more negative (e.g. sad, depressed, angry). |
+        
+        Information about features: [here](https://developer.spotify.com/documentation/web-api/reference/#/operations/get-audio-features)
+        """
 
 def main():
     spr_sidebar()        
