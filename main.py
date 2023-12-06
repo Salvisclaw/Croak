3e4dfb6a844610b35ce8966d7000600331177520
Salvisclaw <88795453+Salvisclaw@users.noreply.github.com>
2023-12-05 23:02:42 -0600

    Update main.py

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ sp = spotipy.client.Spotify(auth_manager=auth_manager)
 
 
 if 'model' not in st.session_state:
-    st.session_state.model = 'Spotify model'
+    st.session_state.model = 'Model 1'
 def update_radio2():
     st.session_state.model=st.session_state.radio2
 if 'genre' not in st.session_state:
@@ -37,7 +37,7 @@ if 'artist' not in st.session_state:
 def update_same_art():
     st.session_state.artist=st.session_state.same_art
 if 'model2' not in st.session_state:
-    st.session_state.model2= 'Model 1'
+    st.session_state.model2= 'Spotify model'
 def update_radio1():
     st.session_state.model2 =st.session_state.radio1
 
