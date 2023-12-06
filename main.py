23d0d328d2648536e99956be75ad161690708b0d
Salvisclaw <88795453+Salvisclaw@users.noreply.github.com>
2023-12-05 23:18:59 -0600

    Update main.py

--- main.py
+++ main.py
@@ -182,7 +182,7 @@ def home_page():
         radio1=col2.radio("Model",options=["Spotify model"],key='radio1',on_change=update_radio1)
         Region=col3.selectbox("Please Choose Region",index=58,key='Region',on_change=update_Region,options=('AD', 'AR', 'AU', 'AT', 'BE', 'BO', 'BR', 'BG', 'CA', 'CL', 'CO', 'CR', 'CY', 'CZ', 'DK', 'DO', 'EC', 'SV', 'EE', 'FI', 'FR', 'DE', 'GR', 'GT', 'HN', 'HK', 'HU', 'IS', 'ID', 'IE', 'IT', 'JP', 'LV', 'LI', 'LT', 'LU', 'MY', 'MT', 'MX', 'MC', 'NL', 'NZ', 'NI', 'NO', 'PA', 'PY', 'PE', 'PH', 'PL', 'PT', 'SG', 'ES', 'SK', 'SE', 'CH', 'TW', 'TR', 'GB', 'US', 'UY'))
     elif radio =="Playlist" or radio =="Song" :
-        radio2=col2.radio("Model",options=("Model 1","Model 2","Spotify Model"),key='radio2',on_change=update_radio2)
+        radio2=col2.radio("Model",options=("Spotify Model","Model 1","Model 2"),key='radio2',on_change=update_radio2)
         if st.session_state.radio2=="Model 1" or st.session_state.radio2=="Model 2":
             num_genre=col3.selectbox("choose a number of genres to focus on",options=(1,2,3,4,5,6,7),index=2,key='num_genre',on_change=update_num_genre)
             same_art=col3.selectbox("How many recommendations by the same artist",options=(1,2,3,4,5,7,10,15),index=3,key='same_art',on_change=update_same_art)
